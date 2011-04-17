f1317449874941bca6acae6a2afd7a8d847314fa
Wakaba <w@suika.fam.cx>
2011-04-17 14:16:31 +0900

    added Char::Prop::Unicode::* modules

---
+++ bin/generate-prop-module.pl
@@ -0,0 +1,221 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $prop = shift;
+my $DEFS = {
+  'Age' => {
+    default_value => 'unassigned',
+    class_name => 'Age',
+    function_name => 'age',
+    prop_name => 'Age',
+    references => [
+      q{UAX #44: Unicode Character Database
+        <http://www.unicode.org/reports/tr44/#Age>.},
+      q{<http://www.unicode.org/Public/UNIDATA/DerivedAge.txt>.},
+    ],
+  },
+  'BidiClass' => {
+    default_value => 'L',
+    class_name => 'BidiClass',
+    function_name => 'bidi_class',
+    prop_name => 'Bidi_Class',
+    references => [
+      q{UAX #9: Unicode Bidirectional Algorithm
+        <http://www.unicode.org/reports/tr9/#Bidirectional_Character_Types>.},
+      q{UAX #44: Unicode Character Database
+        <http://www.unicode.org/reports/tr44/#Bidi_Class>.},
+      q{<http://www.unicode.org/Public/UNIDATA/extracted/DerivedBidiClass.txt>.},
+    ],
+  },
+  '5_1_0::BidiClass' => {
+    default_value => 'L',
+    class_name => '5_1_0::BidiClass',
+    function_name => '5_1_0_bidi_class',
+    prop_name => 'Bidi_Class',
+    references => [
+      q{UAX #9: Unicode Bidirectional Algorithm
+        <http://www.unicode.org/reports/tr9/#Bidirectional_Character_Types>.},
+      q{UAX #44: Unicode Character Database
+        <http://www.unicode.org/reports/tr44/#Bidi_Class>.},
+      q{<http://www.unicode.org/Public/5.1.0/ucd/extracted/DerivedBidiClass.txt>.},
+    ],
+  },
+};
+
+my $DEF = $DEFS->{$prop} or die;
+
+my $map = [];
+my $values = [$DEF->{default_value}];
+my $value_to_index = {$DEF->{default_value} => 0};
+my $unicode_version;
+
+while (<>) {
+  if (/^([0-9A-F]+)\.\.([0-9A-F]+)\s*;\s+(\S+)\s/) {
+    my $from = hex $1;
+    my $to = hex $2;
+    my $value = $3;
+    $value_to_index->{$value} //= do {
+      push @$values, $value;
+      $value_to_index->{$value} = $#$values;
+      $#$values;
+    };
+    $map->[$_] = $value_to_index->{$value} for $from..$to;
+  } elsif (/^([0-9A-F]+)\s+;\s+(\S+)\s/) {
+    my $from = hex $1;
+    my $value = $2;
+    $value_to_index->{$value} //= do {
+      push @$values, $value;
+      $value_to_index->{$value} = $#$values;
+      $#$values;
+    };
+    $map->[$_] = $value_to_index->{$value} for $from;
+  } elsif (not $unicode_version and /^\# \S+?-([0-9.]+)\.txt$/) {
+    $unicode_version = $1;
+  }
+}
+
+use Data::Dumper;
+
+print "package Char::Prop::Unicode::$DEF->{class_name};
+use strict;
+use warnings;
+our \$VERSION = '1.@{[scalar time]}';
+
+## This file is auto-generated (at @{[ sprintf '%04d-%02d-%02dT%02d:%02d:%02dZ', (gmtime)[5]+1900, (gmtime)[4]+1, (gmtime)[3,2,1,0] ]}).
+## Do not edit by hand!
+
+";
+
+if ($unicode_version) {
+  print q{our $UnicodeVersion = '}, $unicode_version, q{';};
+}
+
+print "
+our \$VALUE = ";
+
+my $v = Dumper $values;
+$v =~ s/\$VAR1 = //;
+print $v;
+
+print "our \$DATA = <<'EOH';
+";
+for (0x0000..0x10FFFF) {
+  print pack 'C', 0x40 + ($map->[$_] || 0);
+}
+
+print "
+EOH
+chop \$DATA;
+";
+
+print qq{
+
+use Exporter::Lite;
+our \@EXPORT = qw(
+  unicode_$DEF->{function_name}_n
+  unicode_$DEF->{function_name}_c
+);
+
+no warnings 'uninitialized';
+no warnings 'substr';
+
+sub unicode_$DEF->{function_name}_n (\$) {
+  return \$VALUE->[(unpack 'C', (substr \$DATA, \$_[0], 1 or "\\x40")) - 0x40];
+} # unicode_$DEF->{function_name}_n
+
+sub unicode_$DEF->{function_name}_c (\$) {
+  return \$VALUE->[(unpack 'C', (substr \$DATA, ord \$_[0], 1 or "\\x40")) - 0x40];
+} # unicode_$DEF->{function_name}_c
+
+1;
+
+__END__
+
+=head1 NAME
+
+Char::Prop::Unicode::$DEF->{class_name} - Unicode $unicode_version $DEF->{prop_name} property
+
+=head1 SYNOPSIS
+
+  use Char::Prop::Unicode::$DEF->{class_name};
+  
+  warn unicode_$DEF->{function_name}_n 0x4E00;
+  warn unicode_$DEF->{function_name}_c "\\x{4E00}";
+
+=head1 DESCRIPTION
+
+The C<Char::Prop::Unicode::$DEF->{class_name}> module exports two
+functions, C<unicode_$DEF->{function_name}_n> and
+C<unicode_$DEF->{function_name}_c>, both return the value of Unicode
+$unicode_version $DEF->{prop_name} property for the specified
+character (or code position).  C<unicode_$DEF->{function_name}_n>
+accepts a number representing the code position, while
+C<unicode_$DEF->{function_name}_c> expects a (utf8-flagged) character,
+as the first and only argument.
+
+=head1 UNICODE VERSION
+
+This module supports Unicode $unicode_version.
+
+@{[ $DEF->{class_name} =~ /^\d+/ ? q{
+
+This module will I<not> be updated even when a newer version of
+Unicode is released.
+
+} : q{
+
+This module is expected to be updated once a newer version of Unicode
+is released.
+
+} ]}
+
+=head1 PROPERTY VALUES
+
+The functions return one of following property values:
+
+=over 4
+
+};
+
+for my $value (@$values) {
+  print qq{
+
+=item $value
+
+  };
+}
+
+print qq{
+
+=back
+
+=head1 SEE ALSO
+
+@{[ join "\n\n", map { s/\s+/ /g; $_ } @{$DEF->{references} or []} ]}
+
+=head1 DISTRIBUTION
+
+This module is part of the CharClass project.  The latest version of
+the CharClass package is available from
+<http://suika.fam.cx/www/charclass/readme>.
+
+=head1 AUTHOR
+
+This module is generated by C<@{[[split m{/}, $0]->[-1]]}>, which is written by Wakaba
+<w\@suika.fam.cx>.
+
+The module contains character property data extracted from Unicode
+Data Files distributed by Unicode Consortium under the terms
+<http://www.unicode.org/copyright.html#Exhibit1>.
+
+=head1 LICENSE
+
+Copyright 2011 Wakaba <w\@suika.fam.cx>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+};
---
+++ t/prop/unicode-bidiclass.t
@@ -0,0 +1,98 @@
+package test::Char::Prop::Unicode::BidiClass;
+use strict;
+use warnings;
+use Path::Class;
+use lib file (__FILE__)->dir->parent->parent->subdir ('lib')->stringify;
+use base qw(Test::Class);
+use Char::Prop::Unicode::BidiClass;
+use Test::More;
+no warnings 'utf8';
+
+sub _version : Test(1) {
+  like $Char::Prop::Unicode::BidiClass::UnicodeVersion,
+      qr{\A[0-9]+\.[0-9]+\.[0-9]+\z};
+} # _version
+
+sub _non_unicode : Test(4) {
+  is unicode_bidi_class_n 0x110000, 'L';
+  is unicode_bidi_class_n 0x110001, 'L';
+
+  is unicode_bidi_class_c "\x{110000}", 'L';
+  is unicode_bidi_class_c "\x{110001}", 'L';
+} # _non_unicode
+
+sub _l : Test(4) {
+  is unicode_bidi_class_n 0x0041, 'L';
+  is unicode_bidi_class_n 0x0042, 'L';
+
+  is unicode_bidi_class_c "\x{0041}", 'L';
+  is unicode_bidi_class_c "\x{0042}", 'L';
+} # _l
+
+sub _es : Test(4) {
+  is unicode_bidi_class_n 0xFE62, 'ES';
+  is unicode_bidi_class_n 0xFE63, 'ES';
+
+  is unicode_bidi_class_c "\x{FF0B}", 'ES';
+  is unicode_bidi_class_c "\x{FF0D}", 'ES';
+} # _es
+
+sub _bn : Test(24) {
+  is unicode_bidi_class_n 0x0000, 'BN';
+  is unicode_bidi_class_n 0x0001, 'BN';
+  is unicode_bidi_class_n 0x001A, 'BN';
+  is unicode_bidi_class_n 0x001B, 'BN';
+  is unicode_bidi_class_n 0x007F, 'BN';
+  is unicode_bidi_class_n 0x0080, 'BN';
+  is unicode_bidi_class_n 0x0081, 'BN';
+  is unicode_bidi_class_n 0x0082, 'BN';
+  is unicode_bidi_class_n 0x0083, 'BN';
+  is unicode_bidi_class_n 0x0084, 'BN';
+  is unicode_bidi_class_n 0x0086, 'BN';
+  is unicode_bidi_class_n 0x009F, 'BN';
+  is unicode_bidi_class_n 0x00AD, 'BN';
+  is unicode_bidi_class_n 0x200B, 'BN';
+  is unicode_bidi_class_n 0xFFFE, 'BN';
+  is unicode_bidi_class_n 0xFFFF, 'BN';
+  is unicode_bidi_class_n 0x1D173, 'BN';
+  is unicode_bidi_class_n 0x1D174, 'BN';
+  is unicode_bidi_class_n 0x1FFFE, 'BN';
+  is unicode_bidi_class_n 0x1FFFF, 'BN';
+  is unicode_bidi_class_n 0x10FFFE, 'BN';
+  is unicode_bidi_class_n 0x10FFFF, 'BN';
+
+  is unicode_bidi_class_c "\x{0008}", 'BN';
+  is unicode_bidi_class_c "\x{000E}", 'BN';
+} # _bn
+
+sub _al : Test(8) {
+  is unicode_bidi_class_n 0x0604, 'AL';
+  is unicode_bidi_class_n 0x0605, 'AL';
+  is unicode_bidi_class_n 0x060B, 'AL';
+  is unicode_bidi_class_n 0x060D, 'AL';
+  is unicode_bidi_class_n 0x061B, 'AL';
+  is unicode_bidi_class_n 0x061C, 'AL';
+  is unicode_bidi_class_n 0x061D, 'AL';
+
+  is unicode_bidi_class_c "\x{060B}", 'AL';
+} # _al
+
+sub _cs : Test(3) {
+  is unicode_bidi_class_n 0x002C, 'CS';
+  is unicode_bidi_class_n 0xFF1A, 'CS';
+
+  is unicode_bidi_class_c "\x{002C}", 'CS';
+} # _cs
+
+__PACKAGE__->runtests;
+
+1;
+
+=head1 LICENSE
+
+Copyright 2011 Wakaba <w@suika.fam.cx>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
